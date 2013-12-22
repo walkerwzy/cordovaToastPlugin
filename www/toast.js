891e6e14963416f1603f0d4ed63eefa398abd36e
walker <walker@ctitech.cn>
2013-12-23 01:05:37 +0800

    modify toast.js

--- www/toast.js
+++ www/toast.js
@@ -6,45 +6,28 @@
  * Toast.longshow("Long Toast);
  * Toast.shortshow("Short Toast");
  */
-// var exec = require("cordova/exec");
-
-// var Toast = function () {};
-
-// Toast.prototype.LENGTH_SHORT = 0;
-// Toast.prototype.LENGTH_LONG = 1;
-
-// Toast.prototype._show = function (msg, duration, successCallback, failureCallback) {
-
-//     if (duration.toString().toLowerCase() === 'short') {
-//         duration = this.LENGTH_SHORT;
-//     } else if (duration.toString().toLowerCase() === 'long') {
-//         duration = this.LENGTH_LONG;
-//     }
-
-//     exec(successCallback, failureCallback, 'ToastPlugin', null, [msg, duration]);
-// };
-
-// //Short Toast
-// Toast.prototype.shortshow = function (msg, duration, successCallback, failureCallback) {
-//     this._show(msg, this.LENGTH_SHORT, successCallback, failureCallback);
-// };
-
-// //Long Toast
-// Toast.prototype.longshow = function (msg, duration, successCallback, failureCallback) {
-//     this._show(msg, this.LENGTH_LONG, successCallback, failureCallback);
-// };
-
-// var toast = new Toast();
-// module.exports = toast;
-// window.Toast = toast;
-
-
 var exec = require("cordova/exec");
-module.exports = {
-	longshow : function(msg,successCallback,failureCallback){
-		cordova.exec(successCallback,failureCallback,'ToastPlugin',[{message:msg,duration:1}]);
-	},
-	shortshow : function(msg,successCallback,failureCallback){
-		cordova.exec(successCallback,failureCallback,'ToastPlugin',[{message:msg,duration:0}]);
-	}
+
+var Toast = function () {};
+
+Toast.prototype.LENGTH_SHORT = 0;
+Toast.prototype.LENGTH_LONG = 1;
+
+Toast.prototype._show = function (msg, duration, successCallback, failureCallback) {
+    exec(successCallback, failureCallback, 'ToastPlugin', null, [{message:msg, duration:duration}]);
 };
+
+//Short Toast
+Toast.prototype.shortshow = function (msg, successCallback, failureCallback) {
+    this._show(msg, this.LENGTH_SHORT, successCallback, failureCallback);
+};
+
+//Long Toast
+Toast.prototype.longshow = function (msg, successCallback, failureCallback) {
+    this._show(msg, this.LENGTH_LONG, successCallback, failureCallback);
+};
+
+var toast = new Toast();
+module.exports = toast;
+
+// window.Toast = toast;
